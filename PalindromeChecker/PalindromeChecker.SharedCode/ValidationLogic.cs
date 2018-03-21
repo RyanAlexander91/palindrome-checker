d529549f5714cdc251311bac93d55a6990c59b15
Ryan Alexander <10535959+RyanAlexander91@users.noreply.github.com>
2018-03-21 06:27:18 +0000

    v0.1 Completed

---
+++ PalindromeChecker/PalindromeChecker.SharedCode/ValidationLogic.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalindromeChecker.SharedCode
+{
+    /// <summary>
+    /// Class containing the validation logic for the code
+    /// ----
+    /// For this first example the example code is very simple and does not expect
+    /// things such as captial letters.
+    /// </summary>
+    public class ValidationLogic
+    {
+
+        #region Properties
+
+        private string inputReversed;
+
+        public string InputReversed
+        {
+            get { return inputReversed; }
+            set { inputReversed = value; }
+        }
+
+        #endregion
+
+        #region Validation Code
+        /// <summary>
+        /// Initial Validation Code which can be used in all applications
+        /// </summary>
+        /// <param name="userInput"></param>
+        /// <returns></returns>
+        public bool CheckPalindrome(string userInput)
+        {
+            char[] inputarray = userInput.ToCharArray();
+            Array.Reverse(inputarray);
+            string result = new string(inputarray);
+
+            if (result.Equals(userInput))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        #endregion
+    }
+}
---
+++ PalindromeChecker/PalindromeChecker.UI.Console/Views/MenuView.cs
@@ -0,0 +1,18 @@
+﻿using PalindromeChecker.UI.Console.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeChecker.UI.Console.Views
+{
+    class MenuView
+    {
+        static void Main(string[] args)
+        {
+            var mvm = new MenuViewModel();
+            mvm.LaunchApplication();
+        }
+    }
+}
